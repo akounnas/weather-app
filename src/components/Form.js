b0727cffb1c9e92ae3cee4c33e60d3613caced5d
akounnas <akounnas@gmail.com>
2019-01-26 20:05:08 +0200

    second

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = (props) =>
+(
+    <form onSubmit = {props.getWeather}>
+        <input type = "text" name = "city" placeholder = "City..."></input>
+        <input type = "text" name = "country" placeholder = "Country..."></input>
+        <button>Get Weather</button>
+    </form>
+)
+
+export default Form;
